ff796d8c0bf2b95957f2cef48473903d84af4820
himanshusharma8583 <himanshusharma8583@gmail.com>
2023-05-26 03:25:57 +0530

    update

---
+++ frontend/src/components/ContextReducer.js
@@ -0,0 +1,43 @@
+import React, { createContext, useContext, useReducer } from 'react'
+
+const CartStateContext =createContext();
+const CartDispatchContext = createContext(); 
+
+const reducer = (state,action)=>{
+switch(action.type){
+    case "ADD":
+        return [...state,{id:action.id, name:action.name,qty:action.qty, size:action.size,img:action.img, description:action.description}]
+        
+    case "REMOVE":
+        let newArr = [...state]    
+        newArr.splice(action.index,1)
+        return newArr;
+    case "UPDATE":
+        let arr = [...state]
+        arr.find((food,index)=>{
+            if(food.id === action.id){
+                console.log(food.qty, parseInt(action.qty), action.price + food.price)
+            arr[index] = {...food, qty: parseInt(action.qty)+food.qty, price: action.price+food.price}
+            console.log(arr);
+            }
+            return arr
+        })
+            return arr
+        default:
+            console.log("Error In Reducer");
+}
+}
+
+export const CartProvider = ({children})=>{
+  const[state,dispatch] = useReducer(reducer,[])
+    return(
+            <CartDispatchContext.Provider value = {dispatch}>
+                <CartStateContext.Provider value = {state}>
+                    {children}
+                </CartStateContext.Provider>
+            </CartDispatchContext.Provider>
+    )
+}
+
+export const useCart = ()=>useContext(CartStateContext);
+export const useDispatchCart = ()=> useContext(CartDispatchContext)
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,7 +1,29 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, {useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+import Badge from 'react-bootstrap/Badge'
 import './Navbar.css'
+import Modal from '../../Modal'
+import Cart from '../../screens/Cart/Cart'
+import { useCart } from "../../components/ContextReducer";
+
+
 const Navbar = () => {
+  
+  let data = useCart();
+
+
+  const [cartView, setCartView] = useState(false)
+  const navigate = useNavigate();
+
+  const logoutHandler =()=>{
+    localStorage.removeItem('authToken');
+    navigate("/login")
+  }
+
+
+
+
+
   return (
     <div>
       <nav className="navbar navbar-dark bg-success navbar-expand-lg">
@@ -21,23 +43,47 @@ const Navbar = () => {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
+            <ul className="navbar-nav me-auto">
               <li className="nav-item">
-                <Link className="nav-link " aria-current="page" to="/">
+                <Link className="nav-link active fs-5" aria-current="page" to="/">
                   Home
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/createuser">
-                  SignUp
-                </Link>
-              </li>
+              {(localStorage.getItem("authToken")) ?
+                <li className="nav-item">
+                  <Link className="nav-link active fs-5" aria-current="page" to="/">
+                    My Orders
+                  </Link>
+                </li>
+                : ""
+              }
             </ul>
+
+            {(!localStorage.getItem("authToken")) ?
+            <div className='d-flex'>
+
+              <Link className="btn bg-white text-success mx-1" to="/login">
+                Login
+              </Link>
+              <Link className="btn bg-white text-success mx-1" to="/createuser">
+                SignUp
+              </Link>
+            </div>
+            :
+            <div>
+              <div className='btn bg-white text-success mx-2' onClick={()=>{setCartView(true)}}>My Cart{" "}
+              <Badge pill className='ms-1' bg="danger">{data.length}</Badge>
+              </div>
+              {cartView?<Modal onClose = {()=>setCartView(false)}><Cart/></Modal>:null}
+              <div className='btn fw-bold bg-white text-danger mx-2' onClick={logoutHandler}>Logout</div>
+              </div>
+            
+            }
+
+
+
+
+
           </div>
         </div>
       </nav>
